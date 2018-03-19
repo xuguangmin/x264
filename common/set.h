145a121fe3a42cccae64fd85ad2f9809b13a264c
xuguangmin <345253580@qq.com>
2018-03-19 16:56:48 +0800

    x264_sps_t、x264_pps_t

--- common/set.h
+++ common/set.h
@@ -42,19 +42,21 @@ enum cqm8_e
     CQM_8PC = 3,
 };
 
+/* 序列参数集 */
 typedef struct
 {
-    int i_id;
+    int i_id;	/* sps的id号 */
 
-    int i_profile_idc;
-    int i_level_idc;
+    int i_profile_idc;	/* 指明所用的profile */
+    int i_level_idc;	/* 指明所用的level */
 
+	/* 当他们值等于1时,表示必须遵守某些制约条件 */
     int b_constraint_set0;
     int b_constraint_set1;
     int b_constraint_set2;
     int b_constraint_set3;
 
-    int i_log2_max_frame_num;
+    int i_log2_max_frame_num;	/* 表示图像解码顺序的最大取值 */
 
     int i_poc_type;
     /* poc 0 */
@@ -66,8 +68,9 @@ typedef struct
     int i_mb_height;
     int b_frame_mbs_only;
     int b_mb_adaptive_frame_field;
-    int b_direct8x8_inference;
+    int b_direct8x8_inference;	/* 指明B片的direct和skip模式下运动矢量的预测方法 */
 
+	 /* 图像裁剪参数 */
     int b_crop;
     struct
     {
@@ -142,35 +145,48 @@ typedef struct
 
 } x264_sps_t;
 
+/* 图像参数集 */
 typedef struct
 {
-    int i_id;
-    int i_sps_id;
+    int i_id;	// pps的的序号,在片头被引用
+    int i_sps_id;	// 本图像参数集所引用的序列参数集的序号
 
-    int b_cabac;
+    int b_cabac;	// 是否采用cabac编码,为1就是用cabac,为0则使用calvc
 
+	/* poc的三种计算方法在片层还各需要用一些句法元素作为参数；
+	 * 当等于1时，表示在片头会有句句法元素指明这些参数；
+	 * 当不为1时，表示片头不会给出这些参数 */
     int b_pic_order;
-    int i_num_slice_groups;
-
+    int i_num_slice_groups;	// 加1表示图像中片组的个数
+	
+	// 指明目前参考帧队列的长度，即有多少个参考帧（短期和长期），用于list0
     int i_num_ref_idx_l0_default_active;
+	// 指明目前参考帧队列的长度，即有多少个参考帧（短期和长期），用于list1
     int i_num_ref_idx_l1_default_active;
 
-    int b_weighted_pred;
-    int b_weighted_bipred;
+    int b_weighted_pred;	// 是否开启P帧和SP帧的加权预测
+    int b_weighted_bipred;	// 是否开启B帧的加权预测
 
-    int i_pic_init_qp;
-    int i_pic_init_qs;
+    int i_pic_init_qp;	// 加26后用以指明亮度分量的量化参数的初始值
+    int i_pic_init_qs;	// 同上，只是用于SP和SI
 
+	/* 色度分量的量化参数是根据亮度分量的量化参数计算出来的,
+	 * 本句法元素用以指明计算时用到的参数表示为在 QPC 值的表格中
+	 * 寻找Cb色度分量而应加到参数 QPY 和 QSY 上的偏移。
+	 * 该值应在-12 到 +12范围内(包括边界值) */
     int i_chroma_qp_index_offset;
 
-    int b_deblocking_filter_control;
-    int b_constrained_intra_pred;
+    int b_deblocking_filter_control;	// 编码器可以通过句法元素显式地控制去块滤波的强度
+    int b_constrained_intra_pred;	// 控制PB帧的帧内编码,在P和B帧中,帧内编码的宏块的邻近宏块可能采用帧间编码
+	/* 对于那些属于基本编码图像的条带和条带数据分割块，redundant_pic_cnt等于0；
+	 * 在冗余编码图像中的编码条带和编码条带数据分割块，redundant_pic_cnt大于0；
+	 * 当redundant_pic_cnt 不存在时，默认值为 0；redundant_pic_cnt的值应该在0到127之间 */
     int b_redundant_pic_cnt;
 
     int b_transform_8x8_mode;
 
-    int i_cqm_preset;
-    const uint8_t *scaling_list[8]; /* could be 12, but we don't allow separate Cb/Cr lists */
+    int i_cqm_preset;	// 外部量化矩阵的设置
+    const uint8_t *scaling_list[8];	// 缩放比例列表,could be 12,but we don't allow separate Cb/Cr lists
 
 } x264_pps_t;
 
